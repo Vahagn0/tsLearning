5351d9249fa59305bd8c435bad79cdc4164afe33
Vahagn0 <petrosyanvav@gmail.com>
2023-05-04 16:20:12 +0400

    function signature

---
+++ index.js
@@ -0,0 +1,9 @@
+var func;
+func = function (first, second) {
+    if (first === 1) {
+        return 1;
+    }
+    else {
+        return 2;
+    }
+};
---
+++ index.ts
@@ -0,0 +1,9 @@
+let func : (a: number,b: number) => number
+
+func = (first: number,second: number)=>{
+    if(first === 1){
+        return 1
+    }else{
+        return 2
+    }
+}
